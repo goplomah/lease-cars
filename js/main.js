280aaceae77b618b1c469dff40bb107e93cf86f4
Maks Subochev <maxim.subochev@yandex.ru>
2023-03-19 06:27:35 +0300

    feat: add link filters in section choose

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+const tabsItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabsItem.forEach(element => {
+  element.addEventListener('click', open);
+});
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabsItem.forEach(item => {
+    item.classList.remove('tabs__btn-item_active');
+  });
+
+  tabTarget.classList.add('tabs__btn-item_active');
+
+  tabContent.forEach(item => {
+    item.classList.remove('tabs__content-item_active');
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item_active');
+}
+
+
 const swiper = new Swiper(".swiper", {
     pagination: {
       el: ".swiper-pagination",
